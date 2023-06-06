4e87efb8d2791fa4baff7eef1efb09bc69cc7169
marco <marco.faehndrich@gmail.com>
2023-06-06 08:26:08 +0200

    add general labels, multiple labels per contract

---
+++ scripts/getlabels.py
@@ -0,0 +1,47 @@
+import json
+from os import listdir, path, walk
+
+
+def main():
+    sanc_directory = '../smart-contract-sanctuary-ethereum/contracts/mainnet'
+    label_directories = [
+        '../etherscan-labels-main/data/etherscan/accounts',
+        '../etherscan-labels-main/data/etherscan/tokens'
+    ]
+
+    labels = ['advertising','auction','bridge','charity','closure','donate','defi',
+    'dex','e-commerce','farming','fiat-gateway','fund','layer-2','ico-walltes',
+    'investment','liquidity','mining','news','payment','router','staking']
+
+    contracts = {}
+    for directory in label_directories:
+        for filename in listdir(directory):
+            fileparts = filename.split(".")
+            if fileparts[0] in labels and fileparts[1] == "json":
+                file_path = path.join(directory, filename)
+                with open(file_path) as json_file:
+                    label = path.splitext(filename)[0]
+                    data = json.load(json_file)
+                    for contract_addr, name in data.items():
+                        # print(contract_addr)
+                        if contract_addr.lower() in contracts: 
+                            contracts[contract_addr.lower()]["labels"] += [label]
+                        else:
+                            contracts[contract_addr.lower()] = {"labels": [label]}
+    print("found contracts in etherscan-labels-main: " + str(len(contracts)))
+
+    total_count = 0
+    labelled_contracts = {}
+    for root, directories, files in walk(sanc_directory):
+        for filename in files:
+            total_count += 1
+            contract_addr = "0x" + filename.split("_")[0].lower()
+            # print(contract_addr)
+            if contract_addr in contracts:
+                labelled_contracts[contract_addr] = contracts[contract_addr]
+    print("found contracts in smart-contract-sanctuary-ethereum: " + str(total_count))
+    print("mapped contracts: " + str(len(labelled_contracts)))
+
+
+if __name__ == "__main__":
+    main()
